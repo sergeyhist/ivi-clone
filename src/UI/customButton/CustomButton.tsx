9d75d1a57efcdf4eceae29c640c063861e3adb0a
SergeyHist <sergeyhist@yahoo.com>
2023-04-13 13:34:16 +0300

    fix: customButton component

--- src/UI/customButton/CustomButton.tsx
+++ src/UI/customButton/CustomButton.tsx
@@ -3,38 +3,48 @@ import styles from "./CustomButton.module.sass";
 
 interface CustomButtonProps {
   children: ReactNode;
-  text?: string;
-  icon?: ReactNode;
-  clickCallback: () => void;
-  type?: "purple" | "red" | "icon";
+  clickCallback?: () => void;
+  type?: "purple" | "red";
   padding?: string;
   width?: string;
   height?: string;
   fontSize?: string;
   lineHeight?: string;
+  flex?: string;
+  borderRadius?: string;
 }
 
 const CustomButton: FC<CustomButtonProps> = ({
+  children,
   clickCallback,
   type,
-  children,
   padding,
   width,
   height,
   fontSize,
   lineHeight,
+  flex,
+  borderRadius,
 }) => {
+  const classNames = type
+    ? styles.btn + " " + styles[`btn_${type}`]
+    : styles.btn;
+
+  const styleSelector = {
+    padding: padding ? padding : "9px 15px",
+    flex: flex ? flex : "none",
+    width: width ? width : "auto",
+    height: height ? height : "40px",
+    fontSize: fontSize ? fontSize : "15px",
+    lineHeight: lineHeight ? lineHeight : "20px",
+    borderRadius: borderRadius ? borderRadius : "8px",
+  };
+
   return (
     <button
       onClick={clickCallback}
-      style={{
-        width: width ? width : '',
-        height: height ? height : '',
-        padding: padding ? padding : '',
-        fontSize: fontSize ? fontSize : '',
-        lineHeight: lineHeight ? lineHeight : '',
-      }}
-      className={type ? styles.btn + " " + styles[`btn_${type}`] : styles.btn}
+      style={styleSelector}
+      className={classNames}
     >
       <div className={styles.btn__content}>{children}</div>
     </button>
