4e64b72c992b20faaae87f1db93b2f09efe39d8f
Daniil Stasulevich <alexgrist133@gmail.com>
2023-04-18 13:16:16 +0300

    feat: add header widget to header

--- src/components/Header/DropDown/EntertainmentGenresDropDown/EntertainmentGenresDropDown.tsx
+++ src/components/Header/DropDown/EntertainmentGenresDropDown/EntertainmentGenresDropDown.tsx
@@ -1,7 +1,10 @@
 import {FC} from "react";
 import LinkList from "/src/components/Header/DropDown/LinkList/LinkList";
 import Tabs from "/src/components/Header/DropDown/Tabs/Tabs";
-import {DropDownType} from "/src/components/Header/Header";
+import {DropDownType} from "/src/components/Header/Header.utils";
+import HeaderWidget from "/src/components/Header/HeaderWidget/HeaderWidget";
+import CustomButton from "/src/UI/CustomButton/CustomButton";
+import {IoTvSharp} from 'react-icons/io5';
 
 interface EntertainmentGenresDropDownProps{
     selectedGenre: DropDownType
@@ -13,6 +16,13 @@ const EntertainmentGenresDropDown:FC<EntertainmentGenresDropDownProps> = ({selec
             <LinkList selectedGenre={selectedGenre}/>
             <div>
                 <Tabs selectedGenre={selectedGenre}/>
+            </div>
+            <div>
+                <HeaderWidget/>
+                <CustomButton >
+                    <IoTvSharp/>
+                    Смотреть на Smart TV
+                </CustomButton>
             </div>
         </>
     )
