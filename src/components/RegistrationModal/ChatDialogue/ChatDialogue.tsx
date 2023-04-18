d87e2f71ce86cd572145d58ae8005843e3368f1f
Daniil Stasulevich <alexgrist133@gmail.com>
2023-04-18 10:14:41 +0300

    feat: add placeholder animation

--- src/components/RegistrationModal/ChatDialogue/ChatDialogue.tsx
+++ src/components/RegistrationModal/ChatDialogue/ChatDialogue.tsx
@@ -1,21 +1,39 @@
-import {FC} from "react";
+import {FC, FormEvent, useRef, useState} from "react";
 import styles from './ChatDialogue.module.sass';
 import CustomButton from "/src/UI/CustomButton/CustomButton";
 
 const ChatDialogue:FC = ()=>{
+    const [isInputActive,setIsInputActive] = useState(false);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleInputClick = ():void =>{
+        setIsInputActive(true);
+        if(inputRef.current)
+            inputRef.current.focus();
+    }
+
+    const handleInputBlur = ():void =>{
+        if(inputRef.current.value === '')
+            setIsInputActive(false);
+    }
+
+    const handleSubmitForm = (e: FormEvent):void =>{
+        e.preventDefault();
+    }
+
     return(
-        <form className={styles.chat__form}>
+        <form onSubmit={handleSubmitForm} className={styles.chat__form}>
             <div className={styles.chat__message}>
                 <h3 className={styles.chat__message__title}>Войдите или зарегестрируйтесь</h3>
                 <p className={styles.chat__message__text}>чтобы пользоваться сервисом на любом устройстве</p>
             </div>
             <div>
                 <div className={styles.chat__input__container}>
-                    <div className={styles.chat__input__content}>
-                        <div className={styles.chat__input__placeholder}>Через email</div>
+                    <div className={styles.chat__input__content} onClick={handleInputClick}>
+                        <div className={`${styles.chat__input__placeholder} ${isInputActive ? styles.placeholder_active : ''}`}>Через email</div>
                         <div className={styles.chat__input_block}>
                             <div className={styles.chat__input__icon}></div>
-                            <input className={styles.chat__input} type="text"/>
+                            <input ref={inputRef} className={`${styles.chat__input} ${isInputActive ? styles.input_active : ''}`} onBlur={handleInputBlur} type="email"/>
                         </div>
                     </div>
                     <CustomButton className={styles.chat__button} type="red">
