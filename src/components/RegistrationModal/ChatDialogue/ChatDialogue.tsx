16bd76bf028abaeac640a9c21d0273d0737103f3
Daniil Stasulevich <alexgrist133@gmail.com>
2023-04-18 12:54:43 +0300

    feat: add email validator

--- src/components/RegistrationModal/ChatDialogue/ChatDialogue.tsx
+++ src/components/RegistrationModal/ChatDialogue/ChatDialogue.tsx
@@ -1,27 +1,39 @@
-import {FC, FormEvent, useRef, useState} from "react";
+import {ChangeEvent, FC, FormEvent, useRef, useState} from "react";
 import styles from './ChatDialogue.module.sass';
 import CustomButton from "/src/UI/CustomButton/CustomButton";
+import {validateEmail} from "/src/components/RegistrationModal/ChatDialogue/ChatDoalogue.utils";
+import {RiErrorWarningLine} from 'react-icons/ri';
 
-const ChatDialogue:FC = ()=>{
-    const [isInputActive,setIsInputActive] = useState(false);
+const ChatDialogue: FC = () => {
+    const [isInputActive, setIsInputActive] = useState(false);
+    const [email, setEmail] = useState('');
+    const [isValid, setIsValid] = useState(true);
+
     const inputRef = useRef<HTMLInputElement>(null);
 
-    const handleInputClick = ():void =>{
+    const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const {value} = e.target;
+
+        setEmail(value);
+    }
+
+    const handleInputClick = (): void => {
         setIsInputActive(true);
-        if(inputRef.current)
+        if (inputRef.current)
             inputRef.current.focus();
     }
 
-    const handleInputBlur = ():void =>{
-        if(inputRef.current.value === '')
+    const handleInputBlur = (): void => {
+        if (inputRef.current.value === '')
             setIsInputActive(false);
     }
 
-    const handleSubmitForm = (e: FormEvent):void =>{
+    const handleSubmitForm = (e: FormEvent): void => {
         e.preventDefault();
+        setIsValid(validateEmail(email));
     }
 
-    return(
+    return (
         <form onSubmit={handleSubmitForm} className={styles.chat__form}>
             <div className={styles.chat__message}>
                 <h3 className={styles.chat__message__title}>Войдите или зарегестрируйтесь</h3>
@@ -30,10 +42,19 @@ const ChatDialogue:FC = ()=>{
             <div>
                 <div className={styles.chat__input__container}>
                     <div className={styles.chat__input__content} onClick={handleInputClick}>
-                        <div className={`${styles.chat__input__placeholder} ${isInputActive ? styles.placeholder_active : ''}`}>Через email</div>
+                        <div
+                            className={`${styles.chat__input__placeholder} ${isInputActive ? styles.placeholder_active : ''}`}>Через
+                            email
+                        </div>
                         <div className={styles.chat__input_block}>
                             <div className={styles.chat__input__icon}></div>
-                            <input ref={inputRef} className={`${styles.chat__input} ${isInputActive ? styles.input_active : ''}`} onBlur={handleInputBlur} type="email"/>
+                            <input type="text"
+                                   ref={inputRef}
+                                   value={email}
+                                   className={`${styles.chat__input} ${isInputActive ? styles.input_active : ''}`}
+                                   onBlur={handleInputBlur}
+                                   onChange={handleEmailChange}
+                            />
                         </div>
                     </div>
                     <CustomButton className={styles.chat__button} type="red">
@@ -44,12 +65,25 @@ const ChatDialogue:FC = ()=>{
                     <p>
                         Нажимая «Продолжить», я соглашаюсь
                         <br/>
-                        {`с `}<a className={styles.privacy_policy__link} href="https://www.ivi.ru/info/confidential" target="_blank">Политикой конфиденциальности</a>
+                        {`с `}<a className={styles.privacy_policy__link} href="https://www.ivi.ru/info/confidential"
+                                 target="_blank">Политикой конфиденциальности</a>
                         <br/>
-                        {`и `}<a className={styles.privacy_policy__link} href="https://www.ivi.ru/info/agreement" target="_blank">Пользовательским соглашением</a>
+                        {`и `}<a className={styles.privacy_policy__link} href="https://www.ivi.ru/info/agreement"
+                                 target="_blank">Пользовательским соглашением</a>
                     </p>
                 </div>
             </div>
+            {!isValid &&
+                <div className={styles.chat__error}>
+                    <div className={styles.error__icon}>
+                        <RiErrorWarningLine/>
+                    </div>
+                    <div>
+                        <h3 className={styles.error__title}>Ошибка</h3>
+                        <div className={styles.error__text} >Неправильно указаны данные</div>
+                    </div>
+                </div>
+            }
         </form>
     )
 }
---
+++ src/components/RegistrationModal/ChatDialogue/ChatDoalogue.utils.ts
@@ -0,0 +1,5 @@
+export const validateEmail = (email: string):boolean =>{
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
+    return emailRegex.test(email);
+}
