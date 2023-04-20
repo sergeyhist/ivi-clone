5b464bcfed4bf3f1765db2bdf65efe7aedff3313
Daniil Stasulevich <alexgrist133@gmail.com>
2023-04-20 18:51:22 +0300

    fix: delete registration component from index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,12 @@ import TopTen from "../components/TopTen/TopTen";
 import CinemaDetails from "../components/CinemaDetails/CinemaDetails";
 import HomeSliders from "../components/HomeSliders/HomeSliders";
 import {useTranslation} from "react-i18next";
-import RegistrationModal from "/src/components/RegistrationModal/RegistrationModal";
 
 export default function Home() {
   const { t } = useTranslation();
 
   return (
     <Layout title={t('titles.home')}>
-        <RegistrationModal/>
       <BannerSlider />
       <PromoPlate />
       <TopTen />
