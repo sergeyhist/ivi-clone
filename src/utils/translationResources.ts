29c716becab66ca6cf1ca0c1535e9b4b1140f73a
Nikita Shkutyak <nikitos@iMac-Nikita.local>
2023-04-24 14:14:46 +0400

    feat: delete comments

--- src/utils/translationResources.ts
+++ src/utils/translationResources.ts
@@ -2,9 +2,9 @@ import enHeader from "../locales/en/header.json";
 import enTitles from "../locales/en/titles.json";
 import enFooter from "../locales/en/footer.json";
 import enTabBar from "../locales/en/tabBar.json";
-// import enFilters from "../locales/en/filters.json";
-// import enGenres from "../locales/en/genres.json";
-// import enCountries from "../locales/en/countries.json";
+import enFilters from "../locales/en/filters.json";
+import enGenres from "../locales/en/genres.json";
+import enCountries from "../locales/en/countries.json";
 import enMovie from "../locales/en/movie.json";
 import enBreadcrumbs from "../locales/en/breadcrumbs.json";
 
@@ -12,9 +12,9 @@ import ruHeader from "../locales/ru/header.json";
 import ruTitles from "../locales/ru/titles.json";
 import ruFooter from "../locales/ru/footer.json";
 import ruTabBar from "../locales/ru/tabBar.json";
-// import ruFilters from "../locales/ru/filters.json";
-// import ruGenres from "../locales/ru/genres.json";
-// import ruCountries from "../locales/ru/countries.json";
+import ruFilters from "../locales/ru/filters.json";
+import ruGenres from "../locales/ru/genres.json";
+import ruCountries from "../locales/ru/countries.json";
 import ruMovie from "../locales/ru/movie.json";
 import ruBreadcrumbs from "../locales/ru/breadcrumbs.json";
 
@@ -25,9 +25,9 @@ const translationResources = {
       titles: enTitles,
       footer: enFooter,
       tabBar: enTabBar,
-      // genres: enGenres,
-      // countries: enCountries,
-      // filters: enFilters,
+      genres: enGenres,
+      countries: enCountries,
+      filters: enFilters,
       movie: enMovie,
       breadcrumbs: enBreadcrumbs,
     },
@@ -38,9 +38,9 @@ const translationResources = {
       titles: ruTitles,
       footer: ruFooter,
       tabBar: ruTabBar,
-      // genres: ruGenres,
-      // countries: ruCountries,
-      // filters: ruFilters,
+      genres: ruGenres,
+      countries: ruCountries,
+      filters: ruFilters,
       movie: ruMovie,
       breadcrumbs: ruBreadcrumbs,
     },
