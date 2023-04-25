d5f291cfcb0f24b6f9a31edcc44e91b478c449c2
SergeyHist <sergeyhist@yahoo.com>
2023-04-25 17:22:55 +0300

    refactor: filters info decomposition

--- src/components/FiltersInfo/FiltersInfo.tsx
+++ src/components/FiltersInfo/FiltersInfo.tsx
@@ -1,52 +1,17 @@
 import { FC } from "react";
-import { useTranslation } from "react-i18next";
 import styles from "./FiltersInfo.module.sass";
-import { IActiveFilters, IFilter } from "/src/types/IFilter";
+import { updateTextArray } from "./FiltersInfo.utils";
+import { IActiveFilters } from "/src/types/IFilter";
 
 interface FiltersInfoProps {
   activeFilters: IActiveFilters;
 }
 
 const FiltersInfo: FC<FiltersInfoProps> = ({ activeFilters }) => {
-  const { t } = useTranslation();
-
   const activeFiltersTextArray: string[] = [];
 
-  const getRatingSlug = (key: string) =>
-    ["rating", "ratingCount"].includes(key)
-      ? ` ${(activeFilters[key] as IFilter).slug}`
-      : "";
-
-  const isArrayNotEmpty = (key: string) =>
-    (activeFilters[key] as IFilter[]).length > 0;
-
-  const isFilterNotDefault = (key: string) =>
-    !["all", "0", ""].includes((activeFilters[key] as IFilter).slug);
-
-  const getTextSelector = (key: string) =>
-    (activeFilters[key] as IFilter).text
-      ? t((activeFilters[key] as IFilter).text)
-      : (activeFilters[key] as IFilter).slug;
-
-  const arrayToString = (key: string) =>
-    (activeFilters[key] as IFilter[])
-      .map((filter) => t(filter.text))
-      .join(", ");
-
-  const updateTextArray = (key: string) => {
-    if (!Array.isArray(activeFilters[key]) && isFilterNotDefault(key)) {
-      return activeFiltersTextArray.push(getTextSelector(key) + getRatingSlug(key));
-    }
-
-    if (Array.isArray(activeFilters[key]) && isArrayNotEmpty(key)) {
-      return activeFiltersTextArray.push(arrayToString(key));
-    }
-
-    return activeFiltersTextArray.push(t(`filters.all.${key}`));
-  };
-
   Object.keys(activeFilters).forEach((key) => {
-    updateTextArray(key);
+    activeFiltersTextArray.push(updateTextArray(activeFilters, key));
   });
 
   return <div className={styles.info}>{activeFiltersTextArray.join(", ")}</div>;
