05428ba08c79efea8c17a710b8ecadbfc0c85ba4
Nikita Shkutyak <nikitos.gg.yt@gmail.com>
2023-05-01 12:48:03 +0400

    init: tippyjs

--- src/UI/MovieCard/MovieCardDefault/MovieCardButtons/MovieCardButtons.tsx
+++ src/UI/MovieCard/MovieCardDefault/MovieCardButtons/MovieCardButtons.tsx
@@ -1,15 +1,20 @@
 import { FC } from "react";
 import styles from "./MovieCardButtons.module.sass";
 import { buttonsId } from "../../../../utils/movieCard";
+import { useTranslation } from "next-i18next";
 
 const MovieCardButtons: FC = () => {
+  const { t } = useTranslation("common");
+
   return (
     <div className={styles.buttons}>
       {buttonsId.map((id, index) => (
         <button
           key={index}
-          data-tooltip-id={id}
+          data-tooltip-id={"movie-tooltip"}
           data-tooltip-place={"top"}
+          data-tooltip-delay-show={100}
+          data-tooltip-content={`${t(`tooltips.${id}`)}`}
           className={styles.buttons__button}
         >
           <i className={`${styles[`icon_${id}`]} ${styles.icon}`}></i>
--- src/UI/MovieTooltips/MovieTooltips.tsx
+++ src/UI/MovieTooltips/MovieTooltips.tsx
@@ -1,21 +1,9 @@
 import { FC } from "react";
 import { Tooltip } from "react-tooltip";
 import styles from "./MovieToolTips.module.sass";
-import { useTranslation } from "react-i18next";
-import { buttonsId } from "/src/utils/movieCard";
 
 const MovieTooltips: FC = () => {
-  const { t } = useTranslation("common");
-
-  return (
-    <>
-      {buttonsId.map((id, index) => (
-        <Tooltip key={index} className={styles.tooltip} id={id} place="top">
-          <p>{t(`tooltips.${id}`)}</p>
-        </Tooltip>
-      ))}
-    </>
-  );
+  return <Tooltip className={styles.tooltip} id={"movie-tooltip"} />;
 };
 
 export default MovieTooltips;
