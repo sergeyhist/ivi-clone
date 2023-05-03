0be6465bb528faed61e75586165e58ed2e5f0e7a
Nikita Shkutyak <nikitos.gg.yt@gmail.com>
2023-05-03 14:57:19 +0400

    fix: movie card for backend data

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,5 @@
 import type { StorybookConfig } from "@storybook/nextjs";
-const path = require('path');
+const path = require("path");
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
@@ -12,13 +12,13 @@ const config: StorybookConfig = {
     name: "@storybook/nextjs",
     options: {},
   },
-  webpackFinal: async (config, {configType}) => {
-    config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules'];
+  webpackFinal: async (config: any, { configType }) => {
+    config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules"];
     config.resolve.alias = {
       ...config.resolve.alias,
-      'next-i18next': 'react-i18next'
+      "next-i18next": "react-i18next",
     };
-    return config
+    return config;
   },
   docs: {
     autodocs: "tag",
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,50 +1,51 @@
-import { Suspense } from 'react'
-import { initReactI18next, I18nextProvider } from 'react-i18next'
-import BackEnd from 'i18next-http-backend'
-import i18next from 'i18next'
-import { useGlobals } from '@storybook/addons'
+import { Suspense } from "react";
+import { initReactI18next, I18nextProvider } from "react-i18next";
+import BackEnd from "i18next-http-backend";
+import i18next from "i18next";
+import { useGlobals } from "@storybook/addons";
+import React from "react";
 
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
+  actions: { argTypesRegex: "^on[A-Z].*" },
   controls: { expanded: true },
   storySort: {
-    method: 'alphabetical',
-    order: ['Components', 'Icons', 'Others'],
+    method: "alphabetical",
+    order: ["Components", "Icons", "Others"],
   },
-}
+};
 
 export const globalTypes = {
   locale: {
-    name: 'Locale',
-    description: 'Internationalization locale',
-    defaultValue: 'ru',
+    name: "Locale",
+    description: "Internationalization locale",
+    defaultValue: "ru",
     toolbar: {
-      icon: 'globe',
+      icon: "globe",
       items: [
-        { value: 'en', title: 'English' },
-        { value: 'ru', title: 'Русский' },
+        { value: "en", title: "English" },
+        { value: "ru", title: "Русский" },
       ],
     },
   },
-}
+};
 
 export const decorators = [
   (Story, Context) => {
-    const [{ locale }] = useGlobals()
+    const [{ locale }] = useGlobals();
 
     i18next
-      .use(BackEnd)
-      .use(initReactI18next)
-      .init({
-        lng: locale,
-        fallbackLng: 'en',
-        ns: 'common',
-        defaultNS: 'common',
-        backend: {
-          loadPath: 'locales/{{lng}}/{{ns}}.json',
-        },
-        debug: true,
-      })
+    .use(BackEnd)
+    .use(initReactI18next)
+    .init({
+      lng: locale,
+      fallbackLng: "en",
+      ns: "common",
+      defaultNS: "common",
+      backend: {
+        loadPath: "locales/{{lng}}/{{ns}}.json",
+      },
+      debug: true,
+    });
 
     return (
       <Suspense fallback="Loading...">
@@ -52,6 +53,6 @@ export const decorators = [
           <Story />
         </I18nextProvider>
       </Suspense>
-    )
+    );
   },
-]
+];
