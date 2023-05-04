a7acd294d6d5113096563b29d16009adde205542
Daniil Stasulevich <alexgrist133@gmail.com>
2023-05-04 18:35:02 +0300

    fix: fix styles in navigation

--- src/components/Layout/Header/Navigation/Navigation.tsx
+++ src/components/Layout/Header/Navigation/Navigation.tsx
@@ -18,66 +18,64 @@ const Navigation: FC<NavigationProps> = ({
   return (
     <nav className={styles.nav}>
       <ul className={styles.nav__list}>
-        <Link href="/">
-          <li
-            className={styles.nav__item}
-            onMouseEnter={() => setIsDropdownActive(false)}
-          >
-            {t("navigation.0")}
-          </li>
+        <Link
+          className={styles.nav__item}
+          onMouseEnter={() => setIsDropdownActive(false)}
+          href="/"
+        >
+          <li>{t("navigation.0")}</li>
         </Link>
-        <Link href="https://www.ivi.ru/new" target="_blank">
-          <li
-            className={styles.nav__item}
-            onMouseEnter={() => setIsDropdownActive(false)}
-          >
-            {t("navigation.1")}
-          </li>
+        <Link
+          className={styles.nav__item}
+          onMouseEnter={() => setIsDropdownActive(false)}
+          href="https://www.ivi.ru/new"
+          target="_blank"
+        >
+          <li>{t("navigation.1")}</li>
         </Link>
 
-        <Link href="/movies">
-          <li
-            className={styles.nav__item}
-            onMouseEnter={() => {
-              setIsDropdownActive(true);
-              setDropDownType("movies");
-            }}
-          >
-            {t("navigation.2")}
-          </li>
+        <Link
+          className={styles.nav__item}
+          onMouseEnter={() => {
+            setIsDropdownActive(true);
+            setDropDownType("movies");
+          }}
+          href="/movies"
+        >
+          <li>{t("navigation.2")}</li>
         </Link>
-        <Link href="https://www.ivi.ru/series" target="_blank">
-          <li
-            className={styles.nav__item}
-            onMouseEnter={() => {
-              setIsDropdownActive(true);
-              setDropDownType("series");
-            }}
-          >
-            {t("navigation.3")}
-          </li>
+        <Link
+          className={styles.nav__item}
+          onMouseEnter={() => {
+            setIsDropdownActive(true);
+            setDropDownType("series");
+          }}
+          href="https://www.ivi.ru/series"
+          target="_blank"
+        >
+          <li>{t("navigation.3")}</li>
         </Link>
-        <Link href="https://www.ivi.ru/animation" target="_blank">
-          <li
-            className={styles.nav__item}
-            onMouseEnter={() => {
-              setIsDropdownActive(true);
-              setDropDownType("cartoons");
-            }}
-          >
-            {t("navigation.4")}
-          </li>
+        <Link
+          className={styles.nav__item}
+          onMouseEnter={() => {
+            setIsDropdownActive(true);
+            setDropDownType("cartoons");
+          }}
+          href="https://www.ivi.ru/animation"
+          target="_blank"
+        >
+          <li>{t("navigation.4")}</li>
         </Link>
-        <Link href="https://www.ivi.ru/tvplus" target="_blank">
-          <li
-            className={styles.nav__item}
-            onMouseEnter={() => {
-              setIsDropdownActive(true);
-              setDropDownType("tv");
-            }}
-          >
-            TV+
-          </li>
+        <Link
+          className={styles.nav__item}
+          onMouseEnter={() => {
+            setIsDropdownActive(true);
+            setDropDownType("tv");
+          }}
+          href="https://www.ivi.ru/tvplus"
+          target="_blank"
+        >
+          <li>TV+</li>
         </Link>
       </ul>
     </nav>
