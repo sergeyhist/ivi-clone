d074f7682e92b7536e00c63e8a4772f7c5bfad04
Nikita Shkutyak <nikitos.gg.yt@gmail.com>
2023-05-13 10:44:18 +0400

    feat: add movie translate file in catalog

--- src/UI/MovieCard/MovieCardsChart/MovieCardsChart.tsx
+++ src/UI/MovieCard/MovieCardsChart/MovieCardsChart.tsx
@@ -2,15 +2,18 @@ import { FC } from "react";
 import styles from "./MovieCardsChart.module.sass";
 import { IMovie } from "/src/types/IMovie";
 import ProgressBar from "../../ProgressBar/ProgressBar";
+import { useTranslation } from "next-i18next";
 
 interface MovieCardsChartProps {
   content: IMovie;
 }
 
 const MovieCardsChart: FC<MovieCardsChartProps> = ({ content }) => {
+  const { t } = useTranslation("movie");
+
   return (
     <div className={styles.chart}>
-      <p className={styles.chart__name}>{"Рейтинг"}</p>
+      <p className={styles.chart__name}>{t("rating.name")}</p>
       <ProgressBar value={content.rating * 10} className={styles.chart__bar} />
     </div>
   );
--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -52,6 +52,7 @@ export const getStaticProps = async ({
         "dropDownCategory",
         "year",
         "registration",
+        "movie",
       ])),
     },
   };
