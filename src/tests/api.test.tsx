c499e95054424d820093434616fc7b335ab83e79
Daniil Stasulevich <alexgrist133@gmail.com>
2023-05-18 19:58:02 +0300

    feat: add mockAxios type, adjust eslint config

--- src/tests/api.test.tsx
+++ src/tests/api.test.tsx
@@ -1,34 +1,41 @@
 import axios from "axios";
-import {getUserByEmail} from "/src/api/userApi";
+import { getUserByEmail } from "/src/api/userApi";
 
-jest.mock('axios');
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-describe("userApi",()=>{
+describe("userApi", () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
-  it('should fetch user by email successfully', async () => {
-    const email = 'test@example.com';
-    const userData = { id: 1, name: 'John Doe', email };
 
-    axios.get.mockResolvedValueOnce({ data: userData });
+  it("should fetch user by email successfully", async () => {
+    const email = "test@example.com";
+    const userData = { id: 1, name: "John Doe", email };
+
+    mockedAxios.get.mockResolvedValueOnce({ data: userData });
 
     const result = await getUserByEmail(email);
 
-    expect(axios.get).toHaveBeenCalledTimes(1);
-    expect(axios.get).toHaveBeenCalledWith(`${process.env.SERVER_HOST}/users/${email}`);
+    expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+    expect(mockedAxios.get).toHaveBeenCalledWith(
+      `${String(process.env.SERVER_HOST)}/users/${email}`
+    );
     expect(result).toEqual(userData);
   });
-  it('should handle error and return undefined', async () => {
-    const email = 'test@example.com';
-    const error = new Error('Request failed');
 
-    axios.get.mockRejectedValueOnce(error);
+  it("should handle error and return undefined", async () => {
+    const email = "test@example.com";
+    const error = new Error("Request failed");
+
+    mockedAxios.get.mockRejectedValueOnce(error);
 
     const result = await getUserByEmail(email);
 
-    expect(axios.get).toHaveBeenCalledTimes(1);
-    expect(axios.get).toHaveBeenCalledWith(`${process.env.SERVER_HOST}/users/${email}`);
+    expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+    expect(mockedAxios.get).toHaveBeenCalledWith(
+      `${String(process.env.SERVER_HOST)}/users/${email}`
+    );
     expect(result).toBeUndefined();
   });
-})
+});
