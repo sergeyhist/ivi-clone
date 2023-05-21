d0ba16ef870733267b83c805455de0e3ddd91696
Nikita Shkutyak <nikitos.gg.yt@gmail.com>
2023-05-21 14:53:06 +0400

    fix: header z-index

--- src/components/ModalWindows/MovieModal/MoviePoster/MoviePoster.test.tsx
+++ src/components/ModalWindows/MovieModal/MoviePoster/MoviePoster.test.tsx
@@ -1,26 +1,16 @@
 import React from "react";
 import MoviePoster from "./MoviePoster";
-import { store } from "/src/store";
 import { mockMovie } from "/src/utils/movie";
-import { render } from "@testing-library/react";
-import { Provider } from "react-redux";
+import { renderWithProviders } from "/src/utils/test-utils";
 
 jest.mock("next/router", () => require("next-router-mock"));
 
 describe("MoviePoster", () => {
   it("should open and close without errors", () => {
-    render(
-      <Provider store={store}>
-        <MoviePoster content={mockMovie} />
-      </Provider>
-    );
+    renderWithProviders(<MoviePoster content={mockMovie} />);
   });
   it("should open and close without errors", () => {
     mockMovie.rating = 5;
-    render(
-      <Provider store={store}>
-        <MoviePoster content={mockMovie} />
-      </Provider>
-    );
+    renderWithProviders(<MoviePoster content={mockMovie} />);
   });
 });
