176849d9dc7260fe9ff4f3b2418595cfd5ecc7cc
Daniil Stasulevich <alexgrist133@gmail.com>
2023-05-21 16:20:35 +0300

    feat: adjust next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const { i18n } = require("./next-i18next.config");
 
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   i18n,
   images: {
     domains: ["avatars.mds.yandex.net", "www.avatars.mds.yandex.net"],
