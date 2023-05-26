c589b40d3c182d43175a09e084d936f890f4ef5e
Nikita Shkutyak <nikitos.gg.yt@gmail.com>
2023-05-26 04:32:33 +0400

    feat: comments tests

--- src/components/ModalWindows/MovieModal/CommentTab/CommentsTab.test.tsx
+++ src/components/ModalWindows/MovieModal/CommentTab/CommentsTab.test.tsx
@@ -11,14 +11,14 @@ import { createComment } from "/src/api/comments";
 
 jest.mock("next/router", () => require("next-router-mock"));
 
-jest.mock("../../../../api/user", () => ({
+jest.mock("src/api/user", () => ({
   getUserByEmail: jest.fn(() => jest.fn().mockReturnThis()),
 }));
-jest.mock("../../../../api/comments", () => ({
+jest.mock("src/api/comments", () => ({
   createComment: jest.fn(() => jest.fn().mockReturnThis()),
 }));
 
-const mockSetComments = (): null => null;
+const mockSetComments = jest.fn();
 
 describe("CommentsTab", () => {
   beforeEach(() => {
@@ -29,8 +29,19 @@ describe("CommentsTab", () => {
     act(() =>
       store.dispatch(setAuth({ isLogged: false, userEmail: "admin@gmail.com" }))
     );
-    (getUserByEmail as jest.Mock).mockResolvedValueOnce(Promise.resolve(undefined));
-    (createComment as jest.Mock).mockResolvedValueOnce(Promise.resolve(undefined));
+    render(
+      <Provider store={store}>
+        <CommentsTab comments={[mockComment]} setCommentsState={mockSetComments} />
+      </Provider>
+    );
+    fireEvent.submit(screen.getByTestId("comments-form"));
+    expect(getUserByEmail).not.toBeCalled();
+  });
+
+  it("should return because inputText < 5", () => {
+    act(() =>
+      store.dispatch(setAuth({ isLogged: true, userEmail: "admin@gmail.com" }))
+    );
     render(
       <Provider store={store}>
         <CommentsTab comments={[mockComment]} setCommentsState={mockSetComments} />
@@ -41,6 +52,12 @@ describe("CommentsTab", () => {
   });
 
   it("should return because getUserByEmail return undefined", () => {
+    jest
+    .spyOn(React, "useState")
+    .mockImplementationOnce(() => ["123456", jest.fn()])
+    .mockImplementationOnce(() => [undefined, jest.fn()])
+    .mockImplementationOnce(() => ["placeholder", jest.fn()])
+    .mockImplementationOnce(() => [true, jest.fn()]);
     act(() =>
       store.dispatch(setAuth({ isLogged: true, userEmail: "admin@gmail.com" }))
     );
@@ -52,27 +69,43 @@ describe("CommentsTab", () => {
       </Provider>
     );
     fireEvent.submit(screen.getByTestId("comments-form"));
-    expect(createComment).not.toBeCalled();
+    expect(getUserByEmail).toBeCalled();
+    setTimeout(() => {
+      expect(mockSetComments).not.toBeCalled();
+    }, 1000);
   });
 
   it("should return because createComment response undefined", () => {
+    const setInputText = jest.fn();
+    jest
+    .spyOn(React, "useState")
+    .mockImplementationOnce(() => ["123456", setInputText])
+    .mockImplementationOnce(() => [undefined, jest.fn()])
+    .mockImplementationOnce(() => ["placeholder", jest.fn()])
+    .mockImplementationOnce(() => [true, jest.fn()]);
     (getUserByEmail as jest.Mock).mockResolvedValueOnce(
       Promise.resolve({ user_id: "2" })
     );
     (createComment as jest.Mock).mockResolvedValueOnce(Promise.resolve(undefined));
+    act(() =>
+      store.dispatch(setAuth({ isLogged: true, userEmail: "admin@gmail.com" }))
+    );
     render(
       <Provider store={store}>
         <CommentsTab comments={[mockComment]} setCommentsState={mockSetComments} />
       </Provider>
     );
-    act(() =>
-      store.dispatch(setAuth({ isLogged: true, userEmail: "admin@gmail.com" }))
-    );
     fireEvent.submit(screen.getByTestId("comments-form"));
     expect(getUserByEmail).toBeCalled();
+    setTimeout(() => {
+      expect(mockSetComments).not.toBeCalled();
+    }, 1000);
   });
 
   it("should create comment", () => {
+    jest
+    .spyOn(React, "useState")
+    .mockImplementationOnce(() => ["123456", jest.fn()]);
     (getUserByEmail as jest.Mock).mockResolvedValueOnce(
       Promise.resolve({ user_id: "2" })
     );
@@ -87,6 +120,9 @@ describe("CommentsTab", () => {
     );
     fireEvent.submit(screen.getByTestId("comments-form"));
     expect(getUserByEmail).toBeCalled();
+    setTimeout(() => {
+      expect(mockSetComments).toBeCalled();
+    }, 1000);
   });
 
   it("should create reply comment", () => {
