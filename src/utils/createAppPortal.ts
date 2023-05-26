fd41c6a6e0906d38878d61f5384bf433ee12e82c
Nikita Shkutyak <nikitos.gg.yt@gmail.com>
2023-05-26 14:02:46 +0400

    refactor: delete log

--- src/utils/createAppPortal.ts
+++ src/utils/createAppPortal.ts
@@ -3,13 +3,9 @@ import { createPortal } from "react-dom";
 import { isBrowser } from "./isBrowser";
 
 const createAppPortal = (node: ReactNode): ReactPortal | null => {
-  console.log(isBrowser(window));
-
   const documentRoot = isBrowser(window)
     ? document.getElementById("__next")
     : undefined;
-
-  console.log(documentRoot);
 
   return documentRoot ? createPortal(node, documentRoot) : null;
 };
