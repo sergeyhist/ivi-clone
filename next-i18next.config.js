b0be3f2cab6e0322d400472629a3f53d044d6426
Daniil Stasulevich <alexgrist133@gmail.com>
2023-05-26 11:32:04 +0300

    fix: i18 config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,11 @@
+const path = require('path')
+
 module.exports = {
   i18n: {
     locales: ["en", "ru"],
     defaultLocale: "ru",
-    localeDetection: false
+    localeDetection: false,
+    localePath: path.resolve('./public/locales')
   },
   react: { useSuspense: false },
 };
