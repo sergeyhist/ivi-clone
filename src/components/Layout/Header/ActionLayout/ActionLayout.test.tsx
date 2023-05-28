767e7ebfc7d3694b5f6a28b81d6fe4a0890ab54c
SergeyHist <sergeyhist@yahoo.com>
2023-05-28 11:29:35 +0300

    fix: modalInput test

--- src/components/Layout/Header/ActionLayout/ActionLayout.test.tsx
+++ src/components/Layout/Header/ActionLayout/ActionLayout.test.tsx
@@ -1,5 +1,4 @@
-import { act, fireEvent, screen } from "@testing-library/react";
-import mockRouter from "next-router-mock";
+import { screen } from "@testing-library/react";
 import { renderWithProviders } from "/src/utils/test-utils";
 import Header from "../Header";
 
@@ -9,24 +8,5 @@ describe("ActionLayout", () => {
   it("should renders without errors", () => {
     renderWithProviders(<Header />);
     expect(screen.getByTestId("actionLayout-container")).toBeInTheDocument();
-  });
-
-  it("calls router.push with the correct arguments", () => {
-    jest.useFakeTimers();
-    const push = jest.spyOn(mockRouter, "push");
-
-    mockRouter.locale = "ru";
-    mockRouter.locales = ["en", "ru"];
-    mockRouter.asPath = "/";
-
-    renderWithProviders(<Header />);
-    const toggleSwitch = screen.getByTestId("toggle-switch");
-
-    act(() => fireEvent.click(toggleSwitch));
-    setTimeout(() => {
-      expect(push).toHaveBeenCalled();
-      expect(mockRouter.locale).toEqual("en");
-    }, 500);
-    act(() => jest.runAllTimers());
   });
 });
